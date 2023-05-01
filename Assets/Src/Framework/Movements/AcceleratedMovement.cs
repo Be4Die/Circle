202ff22c746ff49540a8177fe99238d6cd16e5de
Be4Die <misha375375@gmail.com>
2023-05-01 20:38:10 +0400

    Create simple accelerated movement

---
+++ Assets/Src/Framework/Movements/AcceleratedMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UniRx;
+using System;
+
+namespace Framework.Movements
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class AcceleratedMovement : MonoBehaviour, IMoveable
+    {
+        private CompositeDisposable m_movmentDisposable = new CompositeDisposable();
+        private bool m_isInit = false;
+        private Vector2 m_direction;
+        private Rigidbody2D m_rigidbody;
+        private float m_acceleration;
+        [SerializeField]private float m_speed = 0;
+
+        public void Init(Vector2 direction, float startSpeed, float acceleration)
+        {
+            m_direction = direction;
+            m_speed = startSpeed;
+            m_acceleration = acceleration;
+            m_rigidbody = GetComponent<Rigidbody2D>();
+            m_isInit = true;
+        }
+        public void StartMove() 
+        {
+            Observable.EveryFixedUpdate().Subscribe(_ => Move()).AddTo(m_movmentDisposable);
+        }
+        public void StopMoving()
+        {
+            m_movmentDisposable.Clear();
+        }
+
+        public void Move() 
+        {
+            if (m_isInit)
+            {
+                m_speed += m_acceleration * Time.fixedDeltaTime;
+                var newPosition =  m_rigidbody.position + m_direction.normalized * Time.fixedDeltaTime * m_speed;
+                m_rigidbody.MovePosition(newPosition);
+            }
+            else throw new Exception($"{this.GetType()} Error, init class before use");
+        } 
+    }
+}
---
+++ Assets/Src/Framework/Movements/IMoveable.cs
@@ -0,0 +1,7 @@
+﻿namespace Framework.Movements
+{
+    public interface IMoveable
+    {
+        public void Move();
+    }
+}
